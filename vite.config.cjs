80b6b901b3b31bf67f8c3bdca767897672f6e4f4
Joan <js.199911@gmail.com>
2024-01-09 17:46:53 -0500

    Update package.json and vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -10,4 +10,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jscortes23.github.io/newsletter-sign-up-with-success-message-main'
 })
